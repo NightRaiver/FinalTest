475d7cf7d6f6af8e85a001b3e2387c7c69eddbdd
Anastasia Li <sevenreality@yandex.ru>
2021-12-28 05:12:06 +0300

    added task program

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = new int[] { -2, -1, 0, 1, 2 };
+void ArrayOfEvenNumbers (int[] array) {
+    for (int index = 0; index < array.Length; index++)
+    {
+        if (array[index] % 2 == 0) Console.Write($"{array[index]} ");
+    }
+}
+ArrayOfEvenNumbers(array);
